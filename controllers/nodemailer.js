021da5391f2ebca9cd46a6d6bc77c8b75a8510e5
MartinVillegas25 <martinvillegas90@hotmail.com>
2023-08-04 10:17:01 -0300

    configuracion api gmail- nodemailer

--- controllers/nodemailer.js
+++ controllers/nodemailer.js
@@ -1,17 +1,55 @@
 const nodemailer = require('nodemailer');
+const { google } = require("googleapis");
 
-const mailTransporter = nodemailer.createTransport({
-    service: 'tu_servicio_de_correo',
+
+
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+
+oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
+
+//YOU CAN PASS MORE ARGUMENTS TO THIS FUNCTION LIKE CC, TEMPLATES, ATTACHMENTS ETC. IM JUST KEEPING IT SIMPLE
+const sendTestEmail = async (email) => {
+  const ACCESS_TOKEN = await oAuth2Client.getAccessToken();
+  const transport = nodemailer.createTransport({
+    service: "gmail",
     auth: {
-      user: process.env.TUMAIL,
-      pass: process.env.MAIL_PASSWORD,
+      type: "OAuth2",
+      user: process.env.MY_EMAIL,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+      accessToken: ACCESS_TOKEN,
+    },
+    tls: {
+      rejectUnauthorized: true,
     },
   });
 
+  //EMAIL OPTIONS
+  const from = process.env.MY_EMAIL;
+  const subject = "Restaura tu contraseña";
+  const html = `
+    <p>Hey ${email},</p>
+    <p>Para recuperar tu contraseña ingresa a el siguiente link</p>
+    <a target="_black" href="google.com">Recuperar password</a>
+    <p>Thank you</p>
+    `;
+  return new Promise((resolve, reject) => {
+    transport.sendMail({ from, subject, email, html }, (err, info) => {
+      if (err) reject(err);
+      resolve(info);
+    });
+  });
+};
+
+
+
   const recuperarClave = async (req, res) => {
     const { email } = req.body;
-    const urlReset = "https://google.com"
-    // Verificar si el correo está en la base de datos
     const query = 'SELECT * FROM users WHERE email = ?';
     connection.query(query, [email], (err, results) => {
       if (err) {
@@ -20,27 +58,26 @@ const mailTransporter = nodemailer.createTransport({
       }
   
       if (results.length === 0) {
+       
         return res.status(404).json({ message: 'Correo electrónico no encontrado.' });
       }
-  
-      // Generar y enviar el enlace de cambio de contraseña
-      const resetLink = `${urlReset}`; // Reemplaza esto con la URL correcta para cambiar la contraseña
-      const mailOptions = {
-        from: process.env.MAIL, // Reemplaza esto con tu correo electrónico
-        to: email,
-        subject: 'Recuperación de contraseña',
-        text: `Para cambiar tu contraseña, haz clic en el siguiente enlace: ${resetLink}`,
-      };
-  
-      mailTransporter.sendMail(mailOptions, (err) => {
-        if (err) {
-          console.log('Error al enviar el correo electrónico:', err);
-          return res.status(500).json({ message: 'Error al enviar el correo electrónico.' });
+      if(results.length > 0) {
+        try {
+          sendTestEmail(email);
+        } catch (error) {
+          console.log(error);
+          res.sent('error al mandar mail')
         }
-  
-        return res.status(200).json({ message: 'Correo electrónico enviado correctamente.' });
-      });
+      }
     });
   }
 
   module.exports = recuperarClave;
+
+
+  
+
+
+
+
+
